41317a993751148cb88bb8245c23a91e794f02b1
stat <stat@78776fbd-6719-4c1f-9a4c-ebc1d401a228>
2005-11-08 16:53:28 +0000

    corrected error in aws.r   length(dy) was NULL in univariate case
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/aws-svn/trunk@51 78776fbd-6719-4c1f-9a4c-ebc1d401a228

--- R/aws.r
+++ R/aws.r
@@ -281,12 +281,12 @@ lseq<-lseq[1:steps]
 k<-1
 hakt <- hinit
 lambda0<-lambda
-if(hinit>1) lambda0<-1e10 # that removes the stochstic term for the first step
+if(hinit>1) lambda0<-1e50 # that removes the stochstic term for the first step
 #
 #   run single steps to display intermediate results
 #
 while(hakt<=hmax){
-dlw<-rep(2*trunc(hakt)+1,length(dy))
+dlw<-rep(2*trunc(hakt)+1,d)
 if(family=="Gaussian"&scorr[1]>0) lambda0<-lambda0*Spatialvar.gauss(hakt/0.42445/4,h0,d)/Spatialvar.gauss(hakt/0.42445/4,1e-5,d)
 if(family=="Gaussian"&length(sigma2)==n){
 # heteroskedastic Gaussian case
