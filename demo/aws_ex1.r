38dfc66c939bdea5dd7152a35885e3d2d9eada68
stat <stat@78776fbd-6719-4c1f-9a4c-ebc1d401a228>
2006-04-26 16:52:12 +0000

    demos for aws (local constant)
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/aws-svn/trunk@75 78776fbd-6719-4c1f-9a4c-ebc1d401a228

---
+++ demo/aws_ex1.r
@@ -0,0 +1,37 @@
+require(aws)
+
+f1 <- function(x){
+#  
+#    Blocks data (Example 6 from Fan & Gijbels (1996)
+#
+     xj <- c(10,13,15,23,25,40,44,65,76,78,81)/100
+     hj <- c(40,-50,30,-40,50,-42,21,43,-31,21,-42)*.37
+     Kern <- function(x) (1-sign(x))/2
+     apply(Kern(outer(xj,x,"-"))*hj,2,sum)
+     }
+f2 <- function(x) 10*sin(2*pi*1.2/(x+.2))
+
+x <- seq(0,1,length=2048)
+example <- as.integer(readline("Select example: 1 for Blocks data (default),  2 for Smooth example"))
+if(!(example %in% (1:2))) example <- 1 
+u <- switch(example,f1(x),f2(x))
+plot(x,u,type="l",col=2,lwd=2)
+title("Regression function")
+sigma <- readline("Standard deviation of noise:\n Press 'Enter' for sigma=3, otherwise provide value of sigma:")
+if(is.na(as.numeric(sigma))) sigma <- 3 else sigma <- as.numeric(sigma)
+if(sigma <= 0) sigma <- 3
+y <- rnorm(u,u,sigma)
+plot(x,u,type="l",col=2,ylim=range(u,y),lwd=2)
+title("Regression function and data")
+points(x,y)
+hmax <- readline("Maximal bandwidth:\n Press 'Enter' for hmax=250, otherwise provide value of hmax:")
+if(is.na(as.numeric(hmax))) hmax <- 250 else hmax <- as.numeric(hmax)
+if(hmax <= 1) hmax <- 250
+cat("Run aws \n")
+yhat <- aws(y,hmax=hmax,graph=TRUE)
+readline("Press ENTER to show results")
+plot(x,u,type="l",col=2,ylim=range(u,y),lwd=2)
+points(x,y)
+lines(x,yhat$theta,col=3,lwd=2)
+title("Data, fitted and true values")
+rm(f1,f2,x,u,y,sigma,hmax,yhat)
---
+++ demo/aws_ex3.r
@@ -0,0 +1,38 @@
+require(aws)
+fw0 <- function(){
+         xy <- rbind(rep(0:255,256),rep(0:255,rep(256,256)))
+         indw <- c(1:12,29:48,73:100,133:168,209:256)
+         w0 <- matrix(rep(FALSE,256*256),ncol=256)
+         w0[indw,] <- TRUE
+         w0[,indw] <- !w0[,indw]
+         w0 <- w0-.5
+	 z <- (xy[1,]-129)^2+(xy[2,]-129)^2
+         w0[z<=10000&z>=4900] <- 0
+         w0[abs(xy[1,]-xy[2,])<=20&z<4900] <- 0
+	 z <- (xy[1,]-225)^2+2*(xy[2,]-30)^2-(xy[1,]-225)*(xy[2,]-30)
+         w0[z<=625] <- 0
+         w0[z<=625&xy[2,]>27&xy[2,]<31] <- -.5
+         w0[z<=625&xy[1,]>223&xy[1,]<227] <- .5
+	 z <- ((xy[2,]-225)^2+2*(xy[1,]-30)^2)+(xy[2,]-225)*(xy[1,]-30)
+         w0[z<=625] <- 0
+         w0[z<=625&xy[1,]>27&xy[1,]<31] <- -.5
+         w0[z<=625&xy[2,]>223&xy[2,]<227] <- .5
+         w0[((xy[2,]-225)^2+(xy[1,]-225)^2)+1*(xy[2,]-225)*(xy[1,]-225)<=400] <- 0
+         w0[((xy[2,]-30)^2+(xy[1,]-30)^2)<=256] <- 0
+	 w0
+	 }
+w0 <- fw0()
+image(w0,col=gray((0:255)/255))
+title("Original image")
+sigma <- readline("Standard deviation of noise:\n Press 'Enter' for sigma=0.5, otherwise provide value of sigma:")
+if(is.na(as.numeric(sigma))) sigma <- 0.5 else sigma <- as.numeric(sigma)
+if(sigma <= 0) sigma <- 0.1
+y <- w0+rnorm(w0,0,sigma)
+image(y,col=gray((0:255)/255))
+title("Noisy image")
+hmax <- readline("Maximal bandwidth:\n Press 'Enter' for hmax=10, otherwise provide value of hmax:")
+if(is.na(as.numeric(hmax))) hmax <- 10 else hmax <- as.numeric(hmax)
+if(hmax <= 1) hmax <- 10
+cat("Run aws \n")
+yhat <- aws(y,hmax=hmax,graph=TRUE)
+rm(fw0,w0,y,sigma,hmax,yhat)
