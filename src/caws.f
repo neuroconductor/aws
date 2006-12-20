4f7ddad3f51dacdfda8cef6302955ec8e3e1baa5
stat <stat@78776fbd-6719-4c1f-9a4c-ebc1d401a228>
2006-12-20 16:50:20 +0000

    corresponding change in caws.f. This may be the basis for restricting neighborhoods to connected sets ... (makes only sense in 2D and 3 D, does not work sufficiently in 1D due to small number of neighboring points)
    Joerg
    
    
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/aws-svn/trunk@89 78776fbd-6719-4c1f-9a4c-ebc1d401a228

--- src/caws.f
+++ src/caws.f
@@ -303,6 +303,157 @@ C
 C   Perform one iteration in local constant three-variate aws (gridded)
 C
 CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+      subroutine caws2(y,fix,n1,n2,n3,hakt,lambda,theta,bi,bi2,
+     1                bi0,ai,model,kern,skern,spmin,spmax,lwght,wght)
+C   
+C   y        observed values of regression function
+C   n1,n2,n3    design dimensions
+C   hakt     actual bandwidth
+C   lambda   lambda or lambda*sigma2 for Gaussian models
+C   theta    estimates from last step   (input)
+C   bi       \sum  Wi   (output)
+C   ai       \sum  Wi Y     (output)
+C   model    specifies the probablilistic model for the KL-Distance
+C   kern     specifies the location kernel
+C   spmax    specifies the truncation point of the stochastic kernel
+C   wght     scaling factor for second and third dimension (larger values shrink)
+C   
+      implicit logical (a-z)
+      external kldist,lkern
+      real*8 kldist,lkern
+      integer n1,n2,n3,model,kern,skern
+      logical aws,fix(1)
+      real*8 y(1),theta(1),bi(1),bi0(1),ai(1),lambda,spmax,wght(2),
+     1       bi2(1),hakt,lwght(1),spmin,spf
+      integer ih1,ih2,ih3,i1,i2,i3,j1,j2,j3,jw1,jw2,jw3,jwind3,jwind2,
+     1        iind,jind,jind3,jind2,clw1,clw2,clw3,dlw1,dlw2,dlw3
+      real*8 thetai,bii,sij,swj,swj2,swj0,swjy,z1,z2,z3,wj,hakt2,bii0,
+     1       heff,hakt2eff
+      hakt2=hakt*hakt
+      spf=spmax/(spmax-spmin)
+      ih1=hakt
+      aws=lambda.lt.1d40
+C
+C   first calculate location weights
+C
+      ih3=hakt/wght(2)
+      ih2=hakt/wght(1)
+      ih1=hakt
+      if(n3.eq.1) ih3=0
+      if(n2.eq.1) ih2=0
+      hakt2eff=ih1*ih1/1.160397
+      if(n3.eq.1) hakt2eff=ih1*ih1/1.25
+      if(n2.eq.1) hakt2eff=ih1*ih1/1.5625
+      clw1=ih1+1
+      clw2=ih2+1
+      clw3=ih3+1
+      dlw1=ih1+clw1
+      dlw2=ih2+clw2
+      dlw3=ih3+clw3
+      z2=0.d0
+      z3=0.d0
+      DO j3=1,dlw3
+         if(n3.gt.1) THEN
+            z3=(clw3-j3)*wght(2)
+            z3=z3*z3
+            ih2=dsqrt(hakt2-z3)/wght(1)
+            jind3=(j3-1)*dlw1*dlw2
+	 ELSE
+	    jind3=0
+	 END IF
+         DO j2=clw2-ih2,clw2+ih2
+            if(n2.gt.1) THEN
+               z2=(clw2-j2)*wght(1)
+               z2=z3+z2*z2
+               ih1=dsqrt(hakt2-z2)
+               jind2=jind3+(j2-1)*dlw1
+	    ELSE
+	       jind2=0
+	    END IF
+            DO j1=clw1-ih1,clw1+ih1
+C  first stochastic term
+               jind=j1+jind2
+               z1=clw1-j1
+               lwght(jind)=lkern(kern,(z1*z1+z2)/hakt2)
+            END DO
+         END DO
+      END DO
+      call rchkusr()
+      DO i3=1,n3
+         DO i2=1,n2
+             DO i1=1,n1
+	       iind=i1+(i2-1)*n1+(i3-1)*n1*n2
+               IF (fix(iind)) CYCLE
+               heff=0.d0
+C    nothing to do, final estimate is already fixed by control 
+               thetai=theta(iind)
+               bii=bi(iind)/lambda
+C   scaling of sij outside the loop
+               bii0=bi0(iind)
+               swj=0.d0
+	       swj2=0.d0
+               swj0=0.d0
+               swjy=0.d0
+               DO jw3=1,dlw3
+	          j3=jw3-clw3+i3
+	          if(j3.lt.1.or.j3.gt.n3) CYCLE
+		  jwind3=(jw3-1)*dlw1*dlw2
+	          jind3=(j3-1)*n1*n2
+                  z3=(clw3-jw3)*wght(2)
+                  z3=z3*z3
+                  if(n2.gt.1) ih2=dsqrt(hakt2-z3)/wght(1)
+                  DO jw2=clw2-ih2,clw2+ih2
+	             j2=jw2-clw2+i2
+	             if(j2.lt.1.or.j2.gt.n2) CYCLE
+		     jwind2=jwind3+(jw2-1)*dlw1
+	             jind2=(j2-1)*n1+jind3
+                     z2=(clw2-jw2)*wght(1)
+                     z2=z3+z2*z2
+                     ih1=dsqrt(hakt2-z2)
+                     DO jw1=clw1-ih1,clw1+ih1
+C  first stochastic term
+	                j1=jw1-clw1+i1
+	                if(j1.lt.1.or.j1.gt.n1) CYCLE
+                        jind=j1+jind2
+                        z1=(clw1-jw1)
+                        z1=z2+z1*z1
+                        wj=lwght(jw1+jwind2)
+                        swj0=swj0+wj
+                        IF (aws) THEN
+                  sij=bii*kldist(model,thetai,theta(jind),bii0)
+                           IF (sij.gt.spmax) CYCLE
+			   IF (skern.eq.2) THEN
+			      wj=wj*(1.d0-sij)
+			   ELSE
+			IF (sij.gt.spmin) wj=wj*dexp(-spf*(sij-spmin))
+			   ENDIF
+C   if sij <= spmin  this just keeps the location penalty
+C    spmin = 0 corresponds to old choice of K_s 
+C   new kernel is flat in [0,spmin] and then decays exponentially
+                        END IF
+                        heff=dmax1(heff,z1)
+                        swj=swj+wj
+                        swj2=swj2+wj*wj
+                        swjy=swjy+wj*y(jind)
+                     END DO
+                  END DO
+               END DO
+               if(heff.lt.hakt2eff) fix(iind)=TRUE
+               ai(iind)=swjy
+               bi(iind)=swj
+               bi2(iind)=swj2
+               bi0(iind)=swj0
+               call rchkusr()
+            END DO
+         END DO
+      END DO
+      RETURN
+      END
+CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
+C
+C   Perform one iteration in local constant three-variate aws (gridded)
+C
+CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
       subroutine chaws(y,fix,si2,n1,n2,n3,hakt,lambda,theta,bi,bi2,
      1           bi0,vred,ai,model,kern,skern,spmin,spmax,lwght,wght)
 C   
