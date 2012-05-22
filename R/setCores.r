9ab5384b01ed997587e0060a76ba8a8a3d7b3805
stat <stat@78776fbd-6719-4c1f-9a4c-ebc1d401a228>
2012-05-22 15:37:37 +0000

    missing files von Felix
    J.
    
    
    git-svn-id: svn+ssh://localhost/Home/stat/WorkSpace/aws-svn/trunk@152 78776fbd-6719-4c1f-9a4c-ebc1d401a228

---
+++ R/setCores.r
@@ -0,0 +1,25 @@
+setCores <-
+function(n) {
+  # Set number of CPU cores which will be used by the package
+  #
+  #     n number of CPU cores
+
+  if (!missing(n)) {
+    if (is.numeric(n)) {
+      n <- as.integer(ceiling(n))
+      n <- .C('setNumThreads', n = as.integer(n), DUP = FALSE, PACKAGE = "aws")$n
+    }
+  }
+  n <- 0L
+  crTot <- 0L
+  n <- .C('getNumThreads', n = as.integer(n), DUP = FALSE, PACKAGE = "aws")$n
+  if (n <= 1L) {
+    cat("Parallel computation will not perform. CPU cores in use: 1.\n")
+  }
+  else {
+    crTot <- .C('getNumCores', n = as.integer(crTot), DUP = FALSE, PACKAGE = "aws")$n
+    cat("Parallel computation will perform.\n")
+    cat("  Total CPU cores available: ", crTot, ".\n", sep = "")
+    cat("  CPU cores in use: ", n, ".\n", sep = "")
+  }
+}
