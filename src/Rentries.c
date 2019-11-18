c57efae4a012c926b7e5b8c50604834e8decc1e0
JPolzehl <polzehl@wias-berlin.de>
2019-11-18 16:22:41 +0100

    added fortran code related to smoothing from dti

--- src/Rentries.c
+++ src/Rentries.c
@@ -5,3 +5,27 @@ double F77_SUB(fpchisq)(double *x, double *df, int *low_tail, int *give_log) {
    return pchisq(*x,*df,*low_tail,*give_log);
 }
 
+double F77_SUB(besseli)(double *x, double *nu, double *expo) {
+   return bessel_i(*x,*nu,*expo);
+}
+
+double F77_SUB(bessliex)(double *x, double *nu, double *expo, double *bi) {
+   return bessel_i_ex(*x,*nu,*expo, bi);
+}
+
+double F77_SUB(gammaf)(double *x) {
+   return gammafn(*x);
+}
+
+double F77_SUB(digammaf)(double *x) {
+   return digamma(*x);
+}
+
+double F77_SUB(lgammaf)(double *x) {
+   return lgammafn(*x);
+}
+
+void F77_SUB(rpsort3)(double *v, int *ii, int *jj)
+{
+    rPsort(v, *ii, *jj);
+}
