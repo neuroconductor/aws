dc75bff94838208adbdb063cfee3375646f250b7
JPolzehl <polzehl@wias-berlin.de>
2019-11-22 11:56:01 +0100

    code for variance estimation from package dti

--- src/smse3.f
+++ src/smse3.f
@@ -1236,44 +1236,6 @@ C  Approximation by Patnaik (1949)
      1                 fici/cj-fj*dgfi)
       RETURN
       END
-      subroutine paramw3(h,vext,ind,w,n)
-C  compute a description of local weights
-C  h    - bandwidth
-C  vext - vector (length 2) of relative voxel extensions
-C  ind  - integer array dim (3,n) containing relative indices in xyz
-C  w    - vector of corresponding weights
-C  n    - number of positive weights (initial value
-C         (2 int(h)+1)*(2 int(h/vext(1))+1)*(2 int(h/vext(2))+1)
-      integer n,ind(3,n)
-      double precision h,vext(2),w(n)
-      integer i,i1,i2,i3,ih1,ih2,ih3
-      double precision hsq,z1,z2,z3
-      hsq=h*h
-      ih1 = int(h)
-      ih2 = int(h/vext(1))
-      ih3 = int(h/vext(2))
-      i=1
-      DO i1=-ih1,ih1
-         z1=i1*i1
-         DO i2=-ih2,ih2
-            z2=i2*vext(1)
-            z2=z1+z2*z2
-            IF(z2.ge.hsq) CYCLE
-            DO i3=-ih3,ih3
-               z3=i3*vext(2)
-               z3=z2+z3*z3
-               IF(z3.ge.hsq) CYCLE
-               ind(1,i)=i1
-               ind(2,i)=i2
-               ind(3,i)=i3
-               w(i)=1.d0-z3/hsq
-               i=i+1
-            END DO
-         END DO
-      END DO
-      n=i-1
-      RETURN
-      END
       subroutine lkfuls0(h,vext,ind,wght,n)
       implicit none
       integer n,ind(3,n)
--- src/varest.f
+++ src/varest.f
@@ -121,6 +121,7 @@ C$OMP END PARALLEL
 C$OMP FLUSH(yout)
       return
       end
+
       subroutine awslchi2(s,ksi,ni,sigma,vpar,L,mask,n1,n2,n3,ind,
      1      w,nw,minni,wad,sad,lambda,nthreds,iL,work,thn,sigman,ksin,
      2      flb,nfb)
@@ -194,7 +195,7 @@ C   thats the estimated standard deviation of s(i1,i2,i3)
          kval = lambda/nii*(sgi/nii+thi)/(0.1d0/nii*sgi+thi)
 C allow for increased kval for low SNR and small ni
 C correction vanishes for nii -> \infty and thi>0
-C without this the propagation condition is violated for very small SNR 
+C without this the propagation condition is violated for very small SNR
          jj = 0
          DO j=1,nw
             wad(j,thrednr)=0.d0
@@ -658,5 +659,44 @@ C
             END DO
          END DO
       END DO
+      RETURN
+      END
+
+      subroutine paramw3(h,vext,ind,w,n)
+C  compute a description of local weights
+C  h    - bandwidth
+C  vext - vector (length 2) of relative voxel extensions
+C  ind  - integer array dim (3,n) containing relative indices in xyz
+C  w    - vector of corresponding weights
+C  n    - number of positive weights (initial value
+C         (2 int(h)+1)*(2 int(h/vext(1))+1)*(2 int(h/vext(2))+1)
+      integer n,ind(3,n)
+      double precision h,vext(2),w(n)
+      integer i,i1,i2,i3,ih1,ih2,ih3
+      double precision hsq,z1,z2,z3
+      hsq=h*h
+      ih1 = int(h)
+      ih2 = int(h/vext(1))
+      ih3 = int(h/vext(2))
+      i=1
+      DO i1=-ih1,ih1
+         z1=i1*i1
+         DO i2=-ih2,ih2
+            z2=i2*vext(1)
+            z2=z1+z2*z2
+            IF(z2.ge.hsq) CYCLE
+            DO i3=-ih3,ih3
+               z3=i3*vext(2)
+               z3=z2+z3*z3
+               IF(z3.ge.hsq) CYCLE
+               ind(1,i)=i1
+               ind(2,i)=i2
+               ind(3,i)=i3
+               w(i)=1.d0-z3/hsq
+               i=i+1
+            END DO
+         END DO
+      END DO
+      n=i-1
       RETURN
       END
